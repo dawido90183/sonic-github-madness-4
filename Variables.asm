aa9e958e9d7e7ea67d2d5feae92642291712aeec
CarlosIagnecz <carlos.edu.iagnecz@gmail.com>
2025-12-12 14:45:18 -0300

    Spindash DMA Queue

--- Variables.asm
+++ Variables.asm
@@ -395,6 +395,7 @@ v_bg1_scroll_flags_dup:	equ $FFFFFF32
 v_bg2_scroll_flags_dup:	equ $FFFFFF34
 v_bg3_scroll_flags_dup:	equ $FFFFFF36
 
+v_character:	equ $FFFFFF7E	; character
 v_levseldelay:	equ $FFFFFF80	; level select - time until change when up/down is held (2 bytes)
 v_levselitem:	equ $FFFFFF82	; level select - item selected (2 bytes)
 v_levselsound:	equ $FFFFFF84	; level select - sound selected (2 bytes)
--- sonic.asm
+++ sonic.asm
@@ -680,13 +680,7 @@ VBla_08:
 
 		writeVRAM	v_hscrolltablebuffer,$380,vram_hscroll
 		writeVRAM	v_spritetablebuffer,$280,vram_sprites
-		tst.b	(f_sonframechg).w ; has Sonic's sprite changed?
-		beq.s	@nochg		; if not, branch
-
-		writeVRAM	v_sgfx_buffer,$2E0,vram_sonic ; load new Sonic gfx
-		move.b	#0,(f_sonframechg).w
-
-	@nochg:
+		jsr	(Process_DMA).l
 		startZ80
 		movem.l	(v_screenposx).w,d0-d7
 		movem.l	d0-d7,(v_screenposx_dup).w
@@ -729,13 +723,7 @@ VBla_0A:
 		writeVRAM	v_hscrolltablebuffer,$380,vram_hscroll
 		startZ80
 		bsr.w	PalCycle_SS
-		tst.b	(f_sonframechg).w ; has Sonic's sprite changed?
-		beq.s	@nochg		; if not, branch
-
-		writeVRAM	v_sgfx_buffer,$2E0,vram_sonic ; load new Sonic gfx
-		move.b	#0,(f_sonframechg).w
-
-	@nochg:
+		jsr	(Process_DMA).l
 		tst.w	(v_generictimer).w	; is there time left on the demo?
 		beq.w	@end	; if not, return
 		subq.w	#1,(v_generictimer).w	; subtract 1 from time left in demo
@@ -800,12 +788,7 @@ VBla_16:
 		writeVRAM	v_spritetablebuffer,$280,vram_sprites
 		writeVRAM	v_hscrolltablebuffer,$380,vram_hscroll
 		startZ80
-		tst.b	(f_sonframechg).w
-		beq.s	@nochg
-		writeVRAM	v_sgfx_buffer,$2E0,vram_sonic
-		move.b	#0,(f_sonframechg).w
-
-	@nochg:
+		jsr	(Process_DMA).l
 		tst.w	(v_generictimer).w
 		beq.w	@end
 		subq.w	#1,(v_generictimer).w
@@ -1119,6 +1102,97 @@ TilemapToVRAM:
 		dbf	d2,Tilemap_Line	; next line
 		rts	
 ; End of function TilemapToVRAM
+
+; ---------------------------------------------------------------------------
+; Subroutine to load VDP commands into the DMA transfer queue
+;
+; In case you wish to use this queue system outside of the spin dash, this is the
+; registers in which it expects data in:
+; d1.l: Address to data (In 68k address space)
+; d2.w: Destination in VRAM
+; d3.w: Length of data
+; ---------------------------------------------------------------------------
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+
+DMA_68KtoVRAM:				; XREF: LoadSonicDynPLC; LoadDustDynPLC
+		movea.l	(v_sgfx_buffer+$FC).w,a1
+		cmpa.w	#v_sgfx_buffer+$FC,a1	; is the DMA queue full?
+		beq.s	DMA_68KtoVRAM_Done ; return if there's no more room in the buffer
+
+		; piece together some VDP commands and store them for later...
+		move.w	#$9300,d0	; command to specify DMA transfer length & $00FF
+		move.b	d3,d0
+		move.w	d0,(a1)+	; store command
+
+		move.w	#$9400,d0	; command to specify DMA transfer length & $FF00
+		lsr.w	#8,d3
+		move.b	d3,d0
+		move.w	d0,(a1)+	; store command
+
+		move.w	#$9500,d0	; command to specify source address & $0001FE
+		lsr.l	#1,d1
+		move.b	d1,d0
+		move.w	d0,(a1)+	; store command
+
+		move.w	#$9600,d0	; command to specify source address & $01FE00
+		lsr.l	#8,d1
+		move.b	d1,d0
+		move.w	d0,(a1)+	; store command
+
+		move.w	#$9700,d0	; command to specify source address & $FE0000
+		lsr.l	#8,d1
+		move.b	d1,d0
+		move.w	d0,(a1)+	; store command
+
+		andi.l	#$FFFF,d2	; command to specify destination address and begin DMA
+		lsl.l	#2,d2
+		lsr.w	#2,d2
+		swap	d2
+		ori.l	#$40000080,d2 ; set bits to specify VRAM transfer
+		move.l	d2,(a1)+	; store command
+
+		move.l	a1,(v_sgfx_buffer+$FC).w ; set the next free slot address
+		cmpa.w	#v_sgfx_buffer+$FC,a1	; has the end of the queue been reached?
+		beq.s	DMA_68KtoVRAM_Done ; return if there's no more room in the buffer
+		move.w	#0,(a1) ; put a stop token at the end of the used part of the buffer
+
+DMA_68KtoVRAM_Done:			; XREF: DMA_68KtoVRAM
+		rts
+; End of function DMA_68KtoVRAM
+
+; ---------------------------------------------------------------------------
+; Process all VDP commands and then reset the DMA queue when it's done
+; ---------------------------------------------------------------------------
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+
+Process_DMA:
+		lea	(vdp_control_port).l,a5
+		lea	(v_sgfx_buffer).w,a1
+
+Process_DMA_Loop:			; XREF: Process_DMA
+		move.w	(a1)+,d0	; has a stop token been encountered?
+		beq.s	Process_DMA_Done ; branch if we reached a stop token
+
+		; issue a set of VDP commands...
+		move.w	d0,(a5)		; transfer length
+		move.w	(a1)+,(a5)	; transfer length
+		move.w	(a1)+,(a5)	; source address
+		move.w	(a1)+,(a5)	; source address
+		move.w	(a1)+,(a5)	; source address
+		move.w	(a1)+,(a5)	; destination
+		move.w	(a1)+,(a5)	; destination
+		cmpa.w	#v_sgfx_buffer+$FC,a1	; has the end of the queue been reached?
+		bne.s	Process_DMA_Loop ; loop if we haven't reached the end of the buffer
+
+Process_DMA_Done:			; XREF: Process_DMA
+		move.w	#0,(v_sgfx_buffer).w
+		move.l	#v_sgfx_buffer,(v_sgfx_buffer+$FC).w
+		rts
+; End of function Process_DMA
 
 		include	"_inc\Nemesis Decompression.asm"
 
@@ -2922,6 +2996,8 @@ Level_ClrRam:
 		move.w	#$8720,(a6)		; set background colour (line 3; colour 0)
 		move.w	#$8A00+223,(v_hbla_hreg).w ; set palette change position (for water)
 		move.w	(v_hbla_hreg).w,(a6)
+		clr.w	(v_sgfx_buffer).w
+		move.l	#v_sgfx_buffer,(v_sgfx_buffer+$FC).w
 		cmpi.b	#id_LZ,(v_zone).w ; is level LZ?
 		bne.s	Level_LoadPal	; if not, branch
 
@@ -3484,6 +3560,8 @@ loc_47D4:
 		lea	(Nem_TitleCard).l,a0 ; load title card patterns
 		bsr.w	NemDec
 		jsr	(Hud_Base).l
+		clr.w	(v_sgfx_buffer).w
+		move.l	#v_sgfx_buffer,(v_sgfx_buffer+$FC).w
 		enable_ints
 		moveq	#palid_SSResult,d0
 		bsr.w	PalLoad2	; load results screen palette
@@ -3968,6 +4046,8 @@ GM_Ending:
 		move.w	#$8720,(a6)		; set background colour (line 3; colour 0)
 		move.w	#$8A00+223,(v_hbla_hreg).w ; set palette change position (for water)
 		move.w	(v_hbla_hreg).w,(a6)
+		clr.w	(v_sgfx_buffer).w
+		move.l	#v_sgfx_buffer,(v_sgfx_buffer+$FC).w
 		move.w	#30,(v_air).w
 		move.w	#id_EndZ<<8,(v_zone).w ; set level number to 0600 (extra flowers)
 		cmpi.b	#6,(v_emeralds).w ; do you have all 6 emeralds?
