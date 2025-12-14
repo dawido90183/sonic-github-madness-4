71fee1d9f00e5cfbc20ebc8a66f6544f73df3aca
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2025-12-14 20:23:13 -0300

    Mario

--- _maps/Moto Bug.asm
+++ _maps/Moto Bug.asm
@@ -1,35 +1,26 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Moto Bug enemy (GHZ)
-; ---------------------------------------------------------------------------
-Map_Moto_internal:
-		dc.w @moto1-Map_Moto_internal
-		dc.w @moto2-Map_Moto_internal
-		dc.w @moto3-Map_Moto_internal
-		dc.w @smoke1-Map_Moto_internal
-		dc.w @smoke2-Map_Moto_internal
-		dc.w @smoke3-Map_Moto_internal
-		dc.w @blank-Map_Moto_internal
-@moto1:		dc.b 4
-		dc.b $F0, $D, 0, 0, $EC
-		dc.b 0,	$C, 0, 8, $EC
-		dc.b $F8, 1, 0,	$C, $C
-		dc.b 8,	8, 0, $E, $F4
-@moto2:		dc.b 4
-		dc.b $F1, $D, 0, 0, $EC
-		dc.b 1,	$C, 0, 8, $EC
-		dc.b $F9, 1, 0,	$C, $C
-		dc.b 9,	8, 0, $11, $F4
-@moto3:		dc.b 5
-		dc.b $F0, $D, 0, 0, $EC
-		dc.b 0,	$C, 0, $14, $EC
-		dc.b $F8, 1, 0,	$C, $C
-		dc.b 8,	4, 0, $18, $EC
-		dc.b 8,	4, 0, $12, $FC
-@smoke1:	dc.b 1
-		dc.b $FA, 0, 0,	$1A, $10
-@smoke2:	dc.b 1
-		dc.b $FA, 0, 0,	$1B, $10
-@smoke3:	dc.b 1
-		dc.b $FA, 0, 0,	$1C, $10
-@blank:		dc.b 0
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_abo6T:	
+		dc.w SME_abo6T_E-SME_abo6T, SME_abo6T_19-SME_abo6T	
+		dc.w SME_abo6T_24-SME_abo6T, SME_abo6T_2F-SME_abo6T	
+		dc.w SME_abo6T_35-SME_abo6T, SME_abo6T_3B-SME_abo6T	
+		dc.w SME_abo6T_41-SME_abo6T	
+SME_abo6T_E:	dc.b 2	
+		dc.b $C8, $F, 0, 0, $EC	
+		dc.b $E8, $F, 0, $10, $EC	
+SME_abo6T_19:	dc.b 2	
+		dc.b $C0, $F, 0, 0, $EC	
+		dc.b $E0, $F, 0, $10, $EC	
+SME_abo6T_24:	dc.b 2	
+		dc.b $C8, $F, 0, 0, $EC	
+		dc.b $E8, $F, 0, $10, $EC	
+SME_abo6T_2F:	dc.b 1	
+		dc.b $FA, 0, 0, $1A, $10	
+SME_abo6T_35:	dc.b 1	
+		dc.b $FA, 0, 0, $1B, $10	
+SME_abo6T_3B:	dc.b 1	
+		dc.b $FA, 0, 0, $28, $10	
+SME_abo6T_41:	dc.b 0	
 		even
