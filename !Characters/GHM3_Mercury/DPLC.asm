8d00910a6a4aec1fe8572576070a15f770aeeb11
CarlosIagnecz <carlos.edu.iagnecz@gmail.com>
2025-12-16 01:42:01 -0300

    Chars work

--- !Characters/GHM3_Mercury/DPLC.asm
+++ !Characters/GHM3_Mercury/DPLC.asm
@@ -1,179 +1,179 @@
 ; ---------------------------------------------------------------------------
-; Uncompressed graphics loading array for Sonic
+; Uncompressed graphics loading array for Mercury
 ; ---------------------------------------------------------------------------
-SonicDynPLC_internal:
-		dc.w SonPLC_Null-SonicDynPLC_internal
-		dc.w SonPLC_Stand-SonicDynPLC_internal
-		dc.w SonPLC_Wait1-SonicDynPLC_internal
-		dc.w SonPLC_Wait2-SonicDynPLC_internal
-		dc.w SonPLC_Wait3-SonicDynPLC_internal
-		dc.w SonPLC_LookUp-SonicDynPLC_internal
-		dc.w SonPLC_Walk11-SonicDynPLC_internal
-		dc.w SonPLC_Walk12-SonicDynPLC_internal
-		dc.w SonPLC_Walk13-SonicDynPLC_internal
-		dc.w SonPLC_Walk14-SonicDynPLC_internal
-		dc.w SonPLC_Walk15-SonicDynPLC_internal
-		dc.w SonPLC_Walk16-SonicDynPLC_internal
-		dc.w SonPLC_Walk21-SonicDynPLC_internal
-		dc.w SonPLC_Walk22-SonicDynPLC_internal
-		dc.w SonPLC_Walk23-SonicDynPLC_internal
-		dc.w SonPLC_Walk24-SonicDynPLC_internal
-		dc.w SonPLC_Walk25-SonicDynPLC_internal
-		dc.w SonPLC_Walk26-SonicDynPLC_internal
-		dc.w SonPLC_Walk31-SonicDynPLC_internal
-		dc.w SonPLC_Walk32-SonicDynPLC_internal
-		dc.w SonPLC_Walk33-SonicDynPLC_internal
-		dc.w SonPLC_Walk34-SonicDynPLC_internal
-		dc.w SonPLC_Walk35-SonicDynPLC_internal
-		dc.w SonPLC_Walk36-SonicDynPLC_internal
-		dc.w SonPLC_Walk41-SonicDynPLC_internal
-		dc.w SonPLC_Walk42-SonicDynPLC_internal
-		dc.w SonPLC_Walk43-SonicDynPLC_internal
-		dc.w SonPLC_Walk44-SonicDynPLC_internal
-		dc.w SonPLC_Walk45-SonicDynPLC_internal
-		dc.w SonPLC_Walk46-SonicDynPLC_internal
-		dc.w SonPLC_Run11-SonicDynPLC_internal
-		dc.w SonPLC_Run12-SonicDynPLC_internal
-		dc.w SonPLC_Run13-SonicDynPLC_internal
-		dc.w SonPLC_Run14-SonicDynPLC_internal
-		dc.w SonPLC_Run21-SonicDynPLC_internal
-		dc.w SonPLC_Run22-SonicDynPLC_internal
-		dc.w SonPLC_Run23-SonicDynPLC_internal
-		dc.w SonPLC_Run24-SonicDynPLC_internal
-		dc.w SonPLC_Run31-SonicDynPLC_internal
-		dc.w SonPLC_Run32-SonicDynPLC_internal
-		dc.w SonPLC_Run33-SonicDynPLC_internal
-		dc.w SonPLC_Run34-SonicDynPLC_internal
-		dc.w SonPLC_Run41-SonicDynPLC_internal
-		dc.w SonPLC_Run42-SonicDynPLC_internal
-		dc.w SonPLC_Run43-SonicDynPLC_internal
-		dc.w SonPLC_Run44-SonicDynPLC_internal
-		dc.w SonPLC_Roll1-SonicDynPLC_internal
-		dc.w SonPLC_Roll2-SonicDynPLC_internal
-		dc.w SonPLC_Roll3-SonicDynPLC_internal
-		dc.w SonPLC_Roll4-SonicDynPLC_internal
-		dc.w SonPLC_Roll5-SonicDynPLC_internal
-		dc.w SonPLC_Warp1-SonicDynPLC_internal
-		dc.w SonPLC_Warp2-SonicDynPLC_internal
-		dc.w SonPLC_Warp3-SonicDynPLC_internal
-		dc.w SonPLC_Warp4-SonicDynPLC_internal
-		dc.w SonPLC_Stop1-SonicDynPLC_internal
-		dc.w SonPLC_Stop2-SonicDynPLC_internal
-		dc.w SonPLC_Duck-SonicDynPLC_internal
-		dc.w SonPLC_Balance1-SonicDynPLC_internal
-		dc.w SonPLC_Balance2-SonicDynPLC_internal
-		dc.w SonPLC_Float1-SonicDynPLC_internal
-		dc.w SonPLC_Float2-SonicDynPLC_internal
-		dc.w SonPLC_Float3-SonicDynPLC_internal
-		dc.w SonPLC_Float4-SonicDynPLC_internal
-		dc.w SonPLC_Spring-SonicDynPLC_internal
-		dc.w SonPLC_Hang1-SonicDynPLC_internal
-		dc.w SonPLC_Hang2-SonicDynPLC_internal
-		dc.w SonPLC_Leap1-SonicDynPLC_internal
-		dc.w SonPLC_Leap2-SonicDynPLC_internal
-		dc.w SonPLC_Push1-SonicDynPLC_internal
-		dc.w SonPLC_Push2-SonicDynPLC_internal
-		dc.w SonPLC_Push3-SonicDynPLC_internal
-		dc.w SonPLC_Push4-SonicDynPLC_internal
-		dc.w SonPLC_Surf-SonicDynPLC_internal
-		dc.w SonPLC_BubStand-SonicDynPLC_internal
-		dc.w SonPLC_Death-SonicDynPLC_internal
-		dc.w SonPLC_Drown-SonicDynPLC_internal
-		dc.w SonPLC_Burnt-SonicDynPLC_internal
-		dc.w SonPLC_Shrink1-SonicDynPLC_internal
-		dc.w SonPLC_Shrink2-SonicDynPLC_internal
-		dc.w SonPLC_Shrink3-SonicDynPLC_internal
-		dc.w SonPLC_Shrink4-SonicDynPLC_internal
-		dc.w SonPLC_Shrink5-SonicDynPLC_internal
-		dc.w SonPLC_Float1-SonicDynPLC_internal
-		dc.w SonPLC_Float3-SonicDynPLC_internal
-		dc.w SonPLC_Injury-SonicDynPLC_internal
-		dc.w SonPLC_GetAir-SonicDynPLC_internal
-		dc.w SonPLC_WaterSlide-SonicDynPLC_internal
-SonPLC_Null:	dc.b 0
-SonPLC_Stand:	dc.b 4,	$20, 0,	$70, 3,	$20, $B, $20, $E
-SonPLC_Wait1:	dc.b 3,	$50, $11, $50, $17, $20, $1D
-SonPLC_Wait2:	dc.b 3,	$50, $20, $50, $17, $20, $1D
-SonPLC_Wait3:	dc.b 3,	$50, $20, $50, $17, $20, $26
-SonPLC_LookUp:	dc.b 3,	$80, $29, $20, $B, $20,	$E
-SonPLC_Walk11:	dc.b 4,	$70, $32, $50, $3A, $50, $40, $10, $46
-SonPLC_Walk12:	dc.b 2,	$70, $32, $B0, $48
-SonPLC_Walk13:	dc.b 2,	$50, $54, $80, $5A
-SonPLC_Walk14:	dc.b 4,	$50, $54, $50, $63, $50, $69, $10, $6F
-SonPLC_Walk15:	dc.b 2,	$50, $54, $B0, $71
-SonPLC_Walk16:	dc.b 3,	$70, $32, $30, $7D, $50, $81
-SonPLC_Walk21:	dc.b 5,	$50, $87, $50, $8D, $20, $93, $50, $96,	0, $9C
-SonPLC_Walk22:	dc.b 6,	$50, $87, $10, $9D, $30, $9F, $50, $A3,	$30, $A9, 0, $AD
-SonPLC_Walk23:	dc.b 4,	$50, $AE, $10, $B4, $70, $B6, $20, $BE
-SonPLC_Walk24:	dc.b 5,	$50, $C1, $30, $C7, $70, $CB, $20, $D3,	$10, $D6
-SonPLC_Walk25:	dc.b 4,	$50, $C1, $10, $D8, $70, $DA, $20, $E2
-SonPLC_Walk26:	dc.b 5,	$50, $87, $10, $9D, 0, $93, $70, $E5, $20, $ED
-SonPLC_Walk31:	dc.b 4,	$70, $F0, $50, $F8, $10, $FE, $51, 0
-SonPLC_Walk32:	dc.b 2,	$70, $F0, $B1, 6
-SonPLC_Walk33:	dc.b 2,	$51, $12, $81, $18
-SonPLC_Walk34:	dc.b 4,	$51, $12, $51, $21, $11, $27, $51, $29
-SonPLC_Walk35:	dc.b 2,	$51, $12, $B1, $2F
-SonPLC_Walk36:	dc.b 3,	$70, $F0, 1, 6,	$81, $3B
-SonPLC_Walk41:	dc.b 6,	$51, $44, $11, $4A, $11, $4C, $81, $4E,	1, $57,	1, $58
-SonPLC_Walk42:	dc.b 6,	$51, $44, $21, $59, $11, $5C, $11, $5E,	$81, $60, 1, $57
-SonPLC_Walk43:	dc.b 4,	$51, $69, $11, $6F, $81, $71, $11, $7A
-SonPLC_Walk44:	dc.b 5,	$51, $7C, $21, $82, $11, $85, $71, $87,	$21, $8F
-SonPLC_Walk45:	dc.b 4,	$51, $7C, $11, $92, $81, $94, $11, $9D
-SonPLC_Walk46:	dc.b 5,	$51, $44, $81, $9F, $11, $5E, $11, $A8,	1, $57
-SonPLC_Run11:	dc.b 2,	$51, $AA, $B1, $B0
-SonPLC_Run12:	dc.b 2,	$50, $54, $B1, $BC
-SonPLC_Run13:	dc.b 2,	$51, $AA, $B1, $C8
-SonPLC_Run14:	dc.b 2,	$50, $54, $B1, $D4
-SonPLC_Run21:	dc.b 4,	$51, $E0, $11, $E6, $B1, $E8, 1, $F4
-SonPLC_Run22:	dc.b 3,	$51, $F5, $11, $FB, $B1, $FD
-SonPLC_Run23:	dc.b 4,	$51, $E0, $12, 9, $B2, $B, 1, $F4
-SonPLC_Run24:	dc.b 3,	$51, $F5, $11, $FB, $B2, $17
-SonPLC_Run31:	dc.b 2,	$52, $23, $B2, $29
-SonPLC_Run32:	dc.b 2,	$51, $12, $B2, $35
-SonPLC_Run33:	dc.b 2,	$52, $23, $B2, $41
-SonPLC_Run34:	dc.b 2,	$51, $12, $B2, $4D
-SonPLC_Run41:	dc.b 4,	$52, $59, $12, $5F, $B2, $61, 2, $6D
-SonPLC_Run42:	dc.b 2,	$72, $6E, $B2, $76
-SonPLC_Run43:	dc.b 4,	$52, $59, $12, $82, $B2, $84, 2, $6D
-SonPLC_Run44:	dc.b 2,	$72, $6E, $B2, $90
-SonPLC_Roll1:	dc.b 1,	$F2, $9C
-SonPLC_Roll2:	dc.b 1,	$F2, $AC
-SonPLC_Roll3:	dc.b 1,	$F2, $BC
-SonPLC_Roll4:	dc.b 1,	$F2, $CC
-SonPLC_Roll5:	dc.b 1,	$F2, $DC
-SonPLC_Warp1:	dc.b 2,	$B2, $EC, $22, $F8
-SonPLC_Warp2:	dc.b 1,	$F2, $FB
-SonPLC_Warp3:	dc.b 2,	$B3, $B, $23, $17
-SonPLC_Warp4:	dc.b 1,	$F3, $1A
-SonPLC_Stop1:	dc.b 2,	$53, $2A, $B3, $30
-SonPLC_Stop2:	dc.b 4,	$53, $3C, $73, $42, $13, $4A, 3, $4C
-SonPLC_Duck:	dc.b 4,	$13, $4D, $73, $4F, $23, $57, 3, $5A
-SonPLC_Balance1:dc.b 3,	$23, $5B, $23, $5E, $F3, $61
-SonPLC_Balance2:dc.b 3,	$B3, $71, $73, $7D, 0, $71
-SonPLC_Float1:	dc.b 3,	$73, $85, $33, $8D, $23, $91
-SonPLC_Float2:	dc.b 1,	$83, $94
-SonPLC_Float3:	dc.b 3,	$73, $9D, 3, $A5, $33, $A6
-SonPLC_Float4:	dc.b 3,	$73, $AA, $33, $B2, $23, $B6
-SonPLC_Spring:	dc.b 3,	$B3, $B9, $13, $C5, 3, $C7
-SonPLC_Hang1:	dc.b 4,	$B3, $C8, $33, $D4, 3, $D8, 3, $D9
-SonPLC_Hang2:	dc.b 4,	$B3, $DA, $33, $E6, 3, $EA, 3, $EB
-SonPLC_Leap1:	dc.b 5,	$83, $EC, $13, $F5, $53, $F7, $13, $FD,	3, $FF
-SonPLC_Leap2:	dc.b 5,	$84, 0,	$14, 9,	$53, $F7, $13, $FD, 3, $FF
-SonPLC_Push1:	dc.b 2,	$84, $B, $74, $14
-SonPLC_Push2:	dc.b 3,	$84, $1C, $24, $25, $14, $28
-SonPLC_Push3:	dc.b 2,	$84, $2A, $74, $33
-SonPLC_Push4:	dc.b 3,	$84, $1C, $24, $3B, $14, $3E
-SonPLC_Surf:	dc.b 2,	$54, $40, $B4, $46
-SonPLC_BubStand:dc.b 3,	$84, $52, $34, $5B, 4, $5F
-SonPLC_Death:	dc.b 3,	$74, $60, $14, $68, $B4, $6A
-SonPLC_Drown:	dc.b 5,	$74, $76, $14, $7E, $54, $80, $34, $86,	4, $8A
-SonPLC_Burnt:	dc.b 5,	$74, $8B, $14, $7E, $54, $93, $34, $86,	4, $8A
-SonPLC_Shrink1:	dc.b 2,	$24, $99, $F4, $9C
-SonPLC_Shrink2:	dc.b 3,	$24, $AC, $B4, $AF, $24, $BB
-SonPLC_Shrink3:	dc.b 1,	$B4, $BE
-SonPLC_Shrink4:	dc.b 1,	$54, $CA
-SonPLC_Shrink5:	dc.b 1,	$14, $D0
-SonPLC_Injury:	dc.b 3,	$B4, $D2, $14, $DE, $34, $E0
-SonPLC_GetAir:	dc.b 3,	$54, $E4, $B4, $EA, $10, $6D
-SonPLC_WaterSlide:dc.b 2, $F4, $F6, $25, 6
+MercuryDynPLC_internal:
+		dc.w MercPLC_Null-MercuryDynPLC_internal
+		dc.w MercPLC_Stand-MercuryDynPLC_internal
+		dc.w MercPLC_Wait1-MercuryDynPLC_internal
+		dc.w MercPLC_Wait2-MercuryDynPLC_internal
+		dc.w MercPLC_Wait3-MercuryDynPLC_internal
+		dc.w MercPLC_LookUp-MercuryDynPLC_internal
+		dc.w MercPLC_Walk11-MercuryDynPLC_internal
+		dc.w MercPLC_Walk12-MercuryDynPLC_internal
+		dc.w MercPLC_Walk13-MercuryDynPLC_internal
+		dc.w MercPLC_Walk14-MercuryDynPLC_internal
+		dc.w MercPLC_Walk15-MercuryDynPLC_internal
+		dc.w MercPLC_Walk16-MercuryDynPLC_internal
+		dc.w MercPLC_Walk21-MercuryDynPLC_internal
+		dc.w MercPLC_Walk22-MercuryDynPLC_internal
+		dc.w MercPLC_Walk23-MercuryDynPLC_internal
+		dc.w MercPLC_Walk24-MercuryDynPLC_internal
+		dc.w MercPLC_Walk25-MercuryDynPLC_internal
+		dc.w MercPLC_Walk26-MercuryDynPLC_internal
+		dc.w MercPLC_Walk31-MercuryDynPLC_internal
+		dc.w MercPLC_Walk32-MercuryDynPLC_internal
+		dc.w MercPLC_Walk33-MercuryDynPLC_internal
+		dc.w MercPLC_Walk34-MercuryDynPLC_internal
+		dc.w MercPLC_Walk35-MercuryDynPLC_internal
+		dc.w MercPLC_Walk36-MercuryDynPLC_internal
+		dc.w MercPLC_Walk41-MercuryDynPLC_internal
+		dc.w MercPLC_Walk42-MercuryDynPLC_internal
+		dc.w MercPLC_Walk43-MercuryDynPLC_internal
+		dc.w MercPLC_Walk44-MercuryDynPLC_internal
+		dc.w MercPLC_Walk45-MercuryDynPLC_internal
+		dc.w MercPLC_Walk46-MercuryDynPLC_internal
+		dc.w MercPLC_Run11-MercuryDynPLC_internal
+		dc.w MercPLC_Run12-MercuryDynPLC_internal
+		dc.w MercPLC_Run13-MercuryDynPLC_internal
+		dc.w MercPLC_Run14-MercuryDynPLC_internal
+		dc.w MercPLC_Run21-MercuryDynPLC_internal
+		dc.w MercPLC_Run22-MercuryDynPLC_internal
+		dc.w MercPLC_Run23-MercuryDynPLC_internal
+		dc.w MercPLC_Run24-MercuryDynPLC_internal
+		dc.w MercPLC_Run31-MercuryDynPLC_internal
+		dc.w MercPLC_Run32-MercuryDynPLC_internal
+		dc.w MercPLC_Run33-MercuryDynPLC_internal
+		dc.w MercPLC_Run34-MercuryDynPLC_internal
+		dc.w MercPLC_Run41-MercuryDynPLC_internal
+		dc.w MercPLC_Run42-MercuryDynPLC_internal
+		dc.w MercPLC_Run43-MercuryDynPLC_internal
+		dc.w MercPLC_Run44-MercuryDynPLC_internal
+		dc.w MercPLC_Roll1-MercuryDynPLC_internal
+		dc.w MercPLC_Roll2-MercuryDynPLC_internal
+		dc.w MercPLC_Roll3-MercuryDynPLC_internal
+		dc.w MercPLC_Roll4-MercuryDynPLC_internal
+		dc.w MercPLC_Roll5-MercuryDynPLC_internal
+		dc.w MercPLC_Warp1-MercuryDynPLC_internal
+		dc.w MercPLC_Warp2-MercuryDynPLC_internal
+		dc.w MercPLC_Warp3-MercuryDynPLC_internal
+		dc.w MercPLC_Warp4-MercuryDynPLC_internal
+		dc.w MercPLC_Stop1-MercuryDynPLC_internal
+		dc.w MercPLC_Stop2-MercuryDynPLC_internal
+		dc.w MercPLC_Duck-MercuryDynPLC_internal
+		dc.w MercPLC_Balance1-MercuryDynPLC_internal
+		dc.w MercPLC_Balance2-MercuryDynPLC_internal
+		dc.w MercPLC_Float1-MercuryDynPLC_internal
+		dc.w MercPLC_Float2-MercuryDynPLC_internal
+		dc.w MercPLC_Float3-MercuryDynPLC_internal
+		dc.w MercPLC_Float4-MercuryDynPLC_internal
+		dc.w MercPLC_Spring-MercuryDynPLC_internal
+		dc.w MercPLC_Hang1-MercuryDynPLC_internal
+		dc.w MercPLC_Hang2-MercuryDynPLC_internal
+		dc.w MercPLC_Leap1-MercuryDynPLC_internal
+		dc.w MercPLC_Leap2-MercuryDynPLC_internal
+		dc.w MercPLC_Push1-MercuryDynPLC_internal
+		dc.w MercPLC_Push2-MercuryDynPLC_internal
+		dc.w MercPLC_Push3-MercuryDynPLC_internal
+		dc.w MercPLC_Push4-MercuryDynPLC_internal
+		dc.w MercPLC_Surf-MercuryDynPLC_internal
+		dc.w MercPLC_BubStand-MercuryDynPLC_internal
+		dc.w MercPLC_Death-MercuryDynPLC_internal
+		dc.w MercPLC_Drown-MercuryDynPLC_internal
+		dc.w MercPLC_Burnt-MercuryDynPLC_internal
+		dc.w MercPLC_Shrink1-MercuryDynPLC_internal
+		dc.w MercPLC_Shrink2-MercuryDynPLC_internal
+		dc.w MercPLC_Shrink3-MercuryDynPLC_internal
+		dc.w MercPLC_Shrink4-MercuryDynPLC_internal
+		dc.w MercPLC_Shrink5-MercuryDynPLC_internal
+		dc.w MercPLC_Float1-MercuryDynPLC_internal
+		dc.w MercPLC_Float3-MercuryDynPLC_internal
+		dc.w MercPLC_Injury-MercuryDynPLC_internal
+		dc.w MercPLC_GetAir-MercuryDynPLC_internal
+		dc.w MercPLC_WaterSlide-MercuryDynPLC_internal
+MercPLC_Null:	dc.b 0
+MercPLC_Stand:	dc.b 4,	$20, 0,	$70, 3,	$20, $B, $20, $E
+MercPLC_Wait1:	dc.b 3,	$50, $11, $50, $17, $20, $1D
+MercPLC_Wait2:	dc.b 3,	$50, $20, $50, $17, $20, $1D
+MercPLC_Wait3:	dc.b 3,	$50, $20, $50, $17, $20, $26
+MercPLC_LookUp:	dc.b 3,	$80, $29, $20, $B, $20,	$E
+MercPLC_Walk11:	dc.b 4,	$70, $32, $50, $3A, $50, $40, $10, $46
+MercPLC_Walk12:	dc.b 2,	$70, $32, $B0, $48
+MercPLC_Walk13:	dc.b 2,	$50, $54, $80, $5A
+MercPLC_Walk14:	dc.b 4,	$50, $54, $50, $63, $50, $69, $10, $6F
+MercPLC_Walk15:	dc.b 2,	$50, $54, $B0, $71
+MercPLC_Walk16:	dc.b 3,	$70, $32, $30, $7D, $50, $81
+MercPLC_Walk21:	dc.b 5,	$50, $87, $50, $8D, $20, $93, $50, $96,	0, $9C
+MercPLC_Walk22:	dc.b 6,	$50, $87, $10, $9D, $30, $9F, $50, $A3,	$30, $A9, 0, $AD
+MercPLC_Walk23:	dc.b 4,	$50, $AE, $10, $B4, $70, $B6, $20, $BE
+MercPLC_Walk24:	dc.b 5,	$50, $C1, $30, $C7, $70, $CB, $20, $D3,	$10, $D6
+MercPLC_Walk25:	dc.b 4,	$50, $C1, $10, $D8, $70, $DA, $20, $E2
+MercPLC_Walk26:	dc.b 5,	$50, $87, $10, $9D, 0, $93, $70, $E5, $20, $ED
+MercPLC_Walk31:	dc.b 4,	$70, $F0, $50, $F8, $10, $FE, $51, 0
+MercPLC_Walk32:	dc.b 2,	$70, $F0, $B1, 6
+MercPLC_Walk33:	dc.b 2,	$51, $12, $81, $18
+MercPLC_Walk34:	dc.b 4,	$51, $12, $51, $21, $11, $27, $51, $29
+MercPLC_Walk35:	dc.b 2,	$51, $12, $B1, $2F
+MercPLC_Walk36:	dc.b 3,	$70, $F0, 1, 6,	$81, $3B
+MercPLC_Walk41:	dc.b 6,	$51, $44, $11, $4A, $11, $4C, $81, $4E,	1, $57,	1, $58
+MercPLC_Walk42:	dc.b 6,	$51, $44, $21, $59, $11, $5C, $11, $5E,	$81, $60, 1, $57
+MercPLC_Walk43:	dc.b 4,	$51, $69, $11, $6F, $81, $71, $11, $7A
+MercPLC_Walk44:	dc.b 5,	$51, $7C, $21, $82, $11, $85, $71, $87,	$21, $8F
+MercPLC_Walk45:	dc.b 4,	$51, $7C, $11, $92, $81, $94, $11, $9D
+MercPLC_Walk46:	dc.b 5,	$51, $44, $81, $9F, $11, $5E, $11, $A8,	1, $57
+MercPLC_Run11:	dc.b 2,	$51, $AA, $B1, $B0
+MercPLC_Run12:	dc.b 2,	$50, $54, $B1, $BC
+MercPLC_Run13:	dc.b 2,	$51, $AA, $B1, $C8
+MercPLC_Run14:	dc.b 2,	$50, $54, $B1, $D4
+MercPLC_Run21:	dc.b 4,	$51, $E0, $11, $E6, $B1, $E8, 1, $F4
+MercPLC_Run22:	dc.b 3,	$51, $F5, $11, $FB, $B1, $FD
+MercPLC_Run23:	dc.b 4,	$51, $E0, $12, 9, $B2, $B, 1, $F4
+MercPLC_Run24:	dc.b 3,	$51, $F5, $11, $FB, $B2, $17
+MercPLC_Run31:	dc.b 2,	$52, $23, $B2, $29
+MercPLC_Run32:	dc.b 2,	$51, $12, $B2, $35
+MercPLC_Run33:	dc.b 2,	$52, $23, $B2, $41
+MercPLC_Run34:	dc.b 2,	$51, $12, $B2, $4D
+MercPLC_Run41:	dc.b 4,	$52, $59, $12, $5F, $B2, $61, 2, $6D
+MercPLC_Run42:	dc.b 2,	$72, $6E, $B2, $76
+MercPLC_Run43:	dc.b 4,	$52, $59, $12, $82, $B2, $84, 2, $6D
+MercPLC_Run44:	dc.b 2,	$72, $6E, $B2, $90
+MercPLC_Roll1:	dc.b 1,	$F2, $9C
+MercPLC_Roll2:	dc.b 1,	$F2, $AC
+MercPLC_Roll3:	dc.b 1,	$F2, $BC
+MercPLC_Roll4:	dc.b 1,	$F2, $CC
+MercPLC_Roll5:	dc.b 1,	$F2, $DC
+MercPLC_Warp1:	dc.b 2,	$B2, $EC, $22, $F8
+MercPLC_Warp2:	dc.b 1,	$F2, $FB
+MercPLC_Warp3:	dc.b 2,	$B3, $B, $23, $17
+MercPLC_Warp4:	dc.b 1,	$F3, $1A
+MercPLC_Stop1:	dc.b 2,	$53, $2A, $B3, $30
+MercPLC_Stop2:	dc.b 4,	$53, $3C, $73, $42, $13, $4A, 3, $4C
+MercPLC_Duck:	dc.b 4,	$13, $4D, $73, $4F, $23, $57, 3, $5A
+MercPLC_Balance1:dc.b 3,	$23, $5B, $23, $5E, $F3, $61
+MercPLC_Balance2:dc.b 3,	$B3, $71, $73, $7D, 0, $71
+MercPLC_Float1:	dc.b 3,	$73, $85, $33, $8D, $23, $91
+MercPLC_Float2:	dc.b 1,	$83, $94
+MercPLC_Float3:	dc.b 3,	$73, $9D, 3, $A5, $33, $A6
+MercPLC_Float4:	dc.b 3,	$73, $AA, $33, $B2, $23, $B6
+MercPLC_Spring:	dc.b 3,	$B3, $B9, $13, $C5, 3, $C7
+MercPLC_Hang1:	dc.b 4,	$B3, $C8, $33, $D4, 3, $D8, 3, $D9
+MercPLC_Hang2:	dc.b 4,	$B3, $DA, $33, $E6, 3, $EA, 3, $EB
+MercPLC_Leap1:	dc.b 5,	$83, $EC, $13, $F5, $53, $F7, $13, $FD,	3, $FF
+MercPLC_Leap2:	dc.b 5,	$84, 0,	$14, 9,	$53, $F7, $13, $FD, 3, $FF
+MercPLC_Push1:	dc.b 2,	$84, $B, $74, $14
+MercPLC_Push2:	dc.b 3,	$84, $1C, $24, $25, $14, $28
+MercPLC_Push3:	dc.b 2,	$84, $2A, $74, $33
+MercPLC_Push4:	dc.b 3,	$84, $1C, $24, $3B, $14, $3E
+MercPLC_Surf:	dc.b 2,	$54, $40, $B4, $46
+MercPLC_BubStand:dc.b 3,	$84, $52, $34, $5B, 4, $5F
+MercPLC_Death:	dc.b 3,	$74, $60, $14, $68, $B4, $6A
+MercPLC_Drown:	dc.b 5,	$74, $76, $14, $7E, $54, $80, $34, $86,	4, $8A
+MercPLC_Burnt:	dc.b 5,	$74, $8B, $14, $7E, $54, $93, $34, $86,	4, $8A
+MercPLC_Shrink1:	dc.b 2,	$24, $99, $F4, $9C
+MercPLC_Shrink2:	dc.b 3,	$24, $AC, $B4, $AF, $24, $BB
+MercPLC_Shrink3:	dc.b 1,	$B4, $BE
+MercPLC_Shrink4:	dc.b 1,	$54, $CA
+MercPLC_Shrink5:	dc.b 1,	$14, $D0
+MercPLC_Injury:	dc.b 3,	$B4, $D2, $14, $DE, $34, $E0
+MercPLC_GetAir:	dc.b 3,	$54, $E4, $B4, $EA, $10, $6D
+MercPLC_WaterSlide:dc.b 2, $F4, $F6, $25, 6
 		even
--- sonic.asm
+++ sonic.asm
@@ -22,6 +22,8 @@ Revision:	equ 1
 
 ZoneCount:	equ 6	; discrete zones are: GHZ, MZ, SYZ, LZ, SLZ, and SBZ
 
+CharCount: equ 4
+
 ; ===========================================================================
 
 StartOfRom:
@@ -2163,6 +2165,8 @@ Char_Pal:
 
 	pal_char Sonic
 	pal_char GHM3_Guy
+	pal_char GHM3_Mercury
+	pal_char GHM3half_Jupiter
 	; add next char here
 
 ; ---------------------------------------------------------------------------
@@ -2828,6 +2832,8 @@ FinalTitle:
 		move.l	d0,(a1)+
 		dbf	d1,Tit_ClrObj1	; fill palette with 0 (black)
 
+		move.w	#0,(v_character).w ; Reset character
+
 		disable_ints
 		locVRAM	$4000
 		lea	(Nem_TitleFg).l,a0 ; load title screen patterns
@@ -2937,6 +2943,22 @@ Tit_MainLoop:
 ; ===========================================================================
 
 Tit_ChkRegion:
+		btst	#bitA,(v_jpadpress1).w ; is pressing A?
+		beq.s	@nocharswap
+
+		move.b	#sfx_Bumper,d0
+		bsr.w	PlaySound_Special	; play ring sound when code is entered
+
+		addq.w	#4,(v_character).w
+		cmpi.w	#(CharCount)*4,(v_character).w
+		blt.s	@nocharswap
+
+		bsr.w	PlaySound_Special	; play ring sound when code is entered
+
+		move.w	#0,(v_character).w
+
+	@nocharswap:
+
 		tst.b	(v_megadrive).w	; check if the machine is US or Japanese
 		bpl.s	Tit_RegionJap	; if Japanese, branch
 
@@ -2993,14 +3015,11 @@ loc_3230:
 		beq.w	Tit_MainLoop	; if not, branch
 
 Tit_ChkLevSel:
-		move.w	#0,(v_character).w ; testing
 
 		tst.b	(f_levselcheat).w ; check if level select code is on
 		beq.w	PlayLevel	; if not, play level
 		btst	#bitA,(v_jpadhold1).w ; check if A is pressed
 		beq.w	PlayLevel	; if not, play level
-
-		move.w	#4,(v_character).w ; testing
 
 		moveq	#palid_LevelSel,d0
 		bsr.w	PalLoad2	; load level select palette
@@ -7761,6 +7780,8 @@ Sonic_Index:	dc.w Sonic_Main-Sonic_Index
 Char_Map:	; CHAR ADD STUFF
 	dc.l	Map_Sonic
 	dc.l	Map_Sonic
+	dc.l	Map_GHM3_Mercury
+	dc.l	Map_GHM3half_Jupiter
 	; add next char here
 
 Sonic_Main:	; Routine 0
@@ -7935,6 +7956,8 @@ Ani_\name:	include	"!Characters\\\name\\Anim.asm"
 		; CHAR ADD STUFF
 
 		anim_char Sonic
+		anim_char GHM3_Mercury
+		anim_char GHM3half_Jupiter
 
 
 ; ---------------------------------------------------------------------------
@@ -9401,6 +9424,8 @@ DPLC_\name:	include	"!Characters\\\name\\DPLC.asm"
 	; CHAR ADD STUFF
 
 	map_char Sonic
+	map_char GHM3_Mercury
+	map_char GHM3half_Jupiter
 	; add next char here
 
 ; ---------------------------------------------------------------------------
@@ -9413,6 +9438,8 @@ Art_\name:	incbin	"!Characters\\\name\\Art.bin"
 	; CHAR ADD STUFF
 
 	art_char Sonic
+	art_char GHM3_Mercury
+	art_char GHM3half_Jupiter
 	; add next char here
 		even
 
