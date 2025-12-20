3c525c481a822ca598241b4fc94f38801a95840c
GuykeAto89 <95724820+Guyke89@users.noreply.github.com>
2025-12-19 22:45:47 -0300

    anims

--- _anim/Press Start and TM.asm
+++ _anim/Press Start and TM.asm
@@ -2,5 +2,5 @@
 ; Animation script - "TM" and "PRESS START BUTTON" on the title screen
 ; ---------------------------------------------------------------------------
 Ani_PSBTM:	dc.w @flash-Ani_PSBTM
-@flash:		dc.b $1F, 0, 1,	afEnd
+@flash:		dc.b $1F, 0,	afEnd
 		even
