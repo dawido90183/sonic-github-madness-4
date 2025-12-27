a2dbc8efdc11e9833799e965d164e6d52b6e0f08
dawid from da 32bit <tetakkrme6@gmail.com>
2025-12-27 22:00:37 +0100

    fuck rings xp orbs better

--- _maps/Rings.asm
+++ _maps/Rings.asm
@@ -1,29 +1,26 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - rings
-; ---------------------------------------------------------------------------
-Map_Ring_internal:
-		dc.w @front-Map_Ring_internal
-		dc.w @angle1-Map_Ring_internal
-		dc.w @edge-Map_Ring_internal
-		dc.w @angle2-Map_Ring_internal
-		dc.w @sparkle1-Map_Ring_internal
-		dc.w @sparkle2-Map_Ring_internal
-		dc.w @sparkle3-Map_Ring_internal
-		dc.w @sparkle4-Map_Ring_internal
-@front:		dc.b 1
-		dc.b $F8, 5, 0,	0, $F8	; ring front
-@angle1:	dc.b 1
-		dc.b $F8, 5, 0,	4, $F8	; ring angle
-@edge:		dc.b 1
-		dc.b $F8, 1, 0,	8, $FC	; ring perpendicular
-@angle2:	dc.b 1
-		dc.b $F8, 5, 8,	4, $F8	; ring angle
-@sparkle1:	dc.b 1
-		dc.b $F8, 5, 0,	$A, $F8	; sparkle
-@sparkle2:	dc.b 1
-		dc.b $F8, 5, $18, $A, $F8 ; sparkle
-@sparkle3:	dc.b 1
-		dc.b $F8, 5, 8,	$A, $F8	;sparkle
-@sparkle4:	dc.b 1
-		dc.b $F8, 5, $10, $A, $F8 ; sparkle
-		even
+Map_fff2:
+Map_fff2_0: 	dc.w Map_fff2_10-Map_fff2
+Map_fff2_2: 	dc.w Map_fff2_16-Map_fff2
+Map_fff2_4: 	dc.w Map_fff2_1C-Map_fff2
+Map_fff2_6: 	dc.w Map_fff2_22-Map_fff2
+Map_fff2_8: 	dc.w Map_fff2_28-Map_fff2
+Map_fff2_A: 	dc.w Map_fff2_2E-Map_fff2
+Map_fff2_C: 	dc.w Map_fff2_34-Map_fff2
+Map_fff2_E: 	dc.w Map_fff2_3A-Map_fff2
+Map_fff2_10: 	dc.b $1
+	dc.b $F8, $5, $0, $0, $F8
+Map_fff2_16: 	dc.b $1
+	dc.b $F8, $5, $0, $4, $F8
+Map_fff2_1C: 	dc.b $1
+	dc.b $F8, $5, $0, $8, $F8
+Map_fff2_22: 	dc.b $1
+	dc.b $F8, $5, $0, $C, $F8
+Map_fff2_28: 	dc.b $1
+	dc.b $F8, $5, $0, $10, $F8
+Map_fff2_2E: 	dc.b $1
+	dc.b $F8, $5, $0, $14, $F8
+Map_fff2_34: 	dc.b $1
+	dc.b $F8, $5, $0, $18, $F8
+Map_fff2_3A: 	dc.b $1
+	dc.b $F8, $5, $0, $1C, $F8
+	even
