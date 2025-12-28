3df77b601de63428474e47e7a9ccfb0bc3da5d1f
HS/C <carlos.edu.iagnecz@gmail.com>
2025-12-27 21:43:58 -0300

    debug building, why did I remove this in the first place

--- build.bat
+++ build.bat
@@ -7,5 +7,11 @@ rem the om stuff is compiler optimizations
 asm64k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p sonic.asm, ghm4built.bin, ghm4built.sym, sonic.lst >errors.txt
 convsym.exe ghm4built.sym ghm4built.bin -a
 
+rem DEBUG BUILD
+asm64k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p /e __DEBUG__=1 sonic.asm, ghm4built.debug.bin, ghm4built.debug.sym, ghm4built.debug.lst >errors_debug.txt
+convsym.exe ghm4built.debug.sym ghm4built.debug.bin -a
+fixheadr.exe ghm4built.debug.bin
+
+
 fixheadr.exe ghm4built.bin
 Pause
