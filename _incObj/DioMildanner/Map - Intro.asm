2bbd99aea9f0f56671152e087a983fd6b03495fb
HS/C <carlos.edu.iagnecz@gmail.com>
2025-12-31 02:38:28 -0300

    Dio Mildanner Intro Files

---
+++ _incObj/DioMildanner/Map - Intro.asm
@@ -0,0 +1,192 @@
+@internal:
+@internal_0: 	dc.w @internal_1C-@internal
+@internal_2: 	dc.w @internal_27-@internal
+@internal_4: 	dc.w @internal_32-@internal
+@internal_6: 	dc.w @internal_3D-@internal
+@internal_8: 	dc.w @internal_48-@internal
+@internal_A: 	dc.w @internal_58-@internal
+@internal_C: 	dc.w @internal_7C-@internal
+@internal_E: 	dc.w @internal_AF-@internal
+@internal_10: 	dc.w @internal_100-@internal
+@internal_12: 	dc.w @internal_160-@internal
+@internal_14: 	dc.w @internal_1C0-@internal
+@internal_16: 	dc.w @internal_225-@internal
+@internal_18: 	dc.w @internal_28A-@internal
+@internal_1A: 	dc.w @internal_2F4-@internal
+@internal_1C: 	dc.b $2
+	dc.b $EC, $7, $0, $0, $F5
+	dc.b $C, $8, $0, $8, $F5
+@internal_27: 	dc.b $2
+	dc.b $ED, $7, $0, $B, $F5
+	dc.b $D, $8, $0, $13, $F5
+@internal_32: 	dc.b $2
+	dc.b $EC, $7, $0, $16, $F5
+	dc.b $C, $8, $0, $1E, $F5
+@internal_3D: 	dc.b $2
+	dc.b $EC, $7, $0, $21, $F5
+	dc.b $C, $8, $0, $29, $F5
+@internal_48: 	dc.b $3
+	dc.b $E4, $B, $0, $2C, $F1
+	dc.b $4, $9, $0, $38, $F1
+	dc.b $DC, $0, $0, $3E, $F9
+@internal_58: 	dc.b $7
+	dc.b $C7, $C, $0, $3F, $F1
+	dc.b $CF, $B, $0, $43, $F1
+	dc.b $D7, $1, $0, $4F, $E9
+	dc.b $D7, $1, $0, $51, $9
+	dc.b $EF, $B, $0, $53, $F1
+	dc.b $F7, $1, $0, $5F, $9
+	dc.b $F, $4, $0, $61, $F9
+@internal_7C: 	dc.b $A
+	dc.b $DB, $F, $0, $63, $EE
+	dc.b $FB, $F, $0, $73, $EE
+	dc.b $1B, $8, $0, $83, $EE
+	dc.b $D3, $C, $0, $86, $F6
+	dc.b $23, $4, $0, $8A, $F6
+	dc.b $C3, $0, $0, $8C, $FE
+	dc.b $CB, $4, $0, $8D, $6
+	dc.b $DB, $5, $0, $8F, $E
+	dc.b $D3, $4, $0, $93, $16
+	dc.b $DB, $0, $0, $95, $1E
+@internal_AF: 	dc.b $10
+	dc.b $EC, $F, $0, $96, $D8
+	dc.b $C, $4, $0, $A6, $D8
+	dc.b $D4, $E, $0, $A8, $E0
+	dc.b $14, $B, $0, $B4, $E8
+	dc.b $34, $4, $0, $C0, $E8
+	dc.b $CC, $8, $0, $C2, $F0
+	dc.b $C, $C, $0, $C5, $F0
+	dc.b $C4, $4, $0, $C9, $F8
+	dc.b $EC, $B, $0, $CB, $F8
+	dc.b $D4, $A, $0, $D7, $0
+	dc.b $14, $5, $0, $E0, $0
+	dc.b $2C, $0, $0, $E4, $0
+	dc.b $F4, $6, $0, $E5, $10
+	dc.b $C, $1, $0, $EB, $10
+	dc.b $DC, $5, $0, $ED, $18
+	dc.b $EC, $0, $0, $F1, $18
+@internal_100: 	dc.b $13
+	dc.b $EA, $D, $0, $F2, $D9
+	dc.b $FA, $6, $0, $FA, $D9
+	dc.b $DA, $D, $1, $0, $E1
+	dc.b $32, $C, $1, $8, $E1
+	dc.b $D2, $C, $1, $C, $E9
+	dc.b $2, $C, $1, $10, $E9
+	dc.b $12, $F, $1, $14, $E9
+	dc.b $FA, $C, $1, $24, $F1
+	dc.b $A, $C, $1, $28, $F1
+	dc.b $C2, $5, $1, $2C, $F9
+	dc.b $EA, $9, $1, $30, $F9
+	dc.b $DA, $D, $1, $36, $1
+	dc.b $D2, $C, $1, $3E, $9
+	dc.b $2, $4, $1, $42, $9
+	dc.b $12, $5, $1, $44, $9
+	dc.b $F2, $5, $1, $48, $11
+	dc.b $A, $0, $1, $4C, $11
+	dc.b $DA, $8, $1, $4D, $21
+	dc.b $E2, $0, $1, $50, $21
+@internal_160: 	dc.b $13
+	dc.b $F2, $F, $1, $51, $F2
+	dc.b $F2, $5, $1, $61, $12
+	dc.b $2, $2, $1, $65, $12
+	dc.b $12, $E, $1, $68, $EA
+	dc.b $12, $1, $1, $74, $A
+	dc.b $2A, $9, $1, $76, $EA
+	dc.b $32, $0, $1, $7C, $E2
+	dc.b $F2, $8, $1, $7D, $D7
+	dc.b $E2, $D, $1, $80, $DF
+	dc.b $FA, $6, $1, $88, $DF
+	dc.b $12, $0, $1, $8E, $DF
+	dc.b $D2, $D, $1, $8F, $E7
+	dc.b $CA, $8, $1, $97, $F7
+	dc.b $E2, $9, $1, $9A, $FF
+	dc.b $D2, $1, $1, $A0, $7
+	dc.b $D0, $E, $1, $A2, $B
+	dc.b $C8, $8, $1, $AE, $1B
+	dc.b $C0, $0, $1, $B1, $23
+	dc.b $D0, $1, $1, $B2, $2B
+@internal_1C0: 	dc.b $14
+	dc.b $2F, $C, $1, $B4, $E3
+	dc.b $17, $D, $1, $B8, $EB
+	dc.b $27, $4, $1, $C0, $EB
+	dc.b $EF, $F, $1, $C2, $F3
+	dc.b $F, $C, $1, $D2, $F3
+	dc.b $17, $4, $1, $D6, $B
+	dc.b $1F, $0, $1, $D8, $B
+	dc.b $F7, $3, $1, $D9, $13
+	dc.b $F2, $8, $1, $7D, $D7
+	dc.b $E2, $D, $1, $80, $DF
+	dc.b $FA, $6, $1, $88, $DF
+	dc.b $12, $0, $1, $8E, $DF
+	dc.b $D2, $D, $1, $8F, $E7
+	dc.b $CA, $8, $1, $97, $F7
+	dc.b $E2, $9, $1, $9A, $FF
+	dc.b $D2, $1, $1, $A0, $7
+	dc.b $D0, $E, $1, $A2, $B
+	dc.b $C8, $8, $1, $AE, $1B
+	dc.b $C0, $0, $1, $B1, $23
+	dc.b $D0, $1, $1, $B2, $2B
+@internal_225: 	dc.b $14
+	dc.b $2F, $C, $1, $B4, $E3
+	dc.b $17, $D, $1, $B8, $EB
+	dc.b $27, $4, $1, $C0, $EB
+	dc.b $EF, $F, $1, $C2, $F3
+	dc.b $F, $C, $1, $D2, $F3
+	dc.b $17, $4, $1, $D6, $B
+	dc.b $1F, $0, $1, $D8, $B
+	dc.b $F7, $3, $1, $D9, $13
+	dc.b $F2, $8, $1, $7D, $D7
+	dc.b $E2, $D, $1, $80, $DF
+	dc.b $FA, $6, $1, $88, $DF
+	dc.b $12, $0, $1, $8E, $DF
+	dc.b $D2, $D, $1, $8F, $E7
+	dc.b $CA, $8, $1, $97, $F7
+	dc.b $E2, $9, $1, $9A, $FF
+	dc.b $D2, $1, $1, $A0, $7
+	dc.b $D2, $D, $1, $DD, $5
+	dc.b $E2, $8, $1, $E5, $15
+	dc.b $C2, $1, $1, $E8, $1D
+	dc.b $CA, $2, $1, $EA, $25
+@internal_28A: 	dc.b $15
+	dc.b $2F, $C, $1, $B4, $E3
+	dc.b $17, $D, $1, $B8, $EB
+	dc.b $27, $4, $1, $C0, $EB
+	dc.b $EF, $F, $1, $C2, $F3
+	dc.b $F, $C, $1, $D2, $F3
+	dc.b $17, $4, $1, $D6, $B
+	dc.b $1F, $0, $1, $D8, $B
+	dc.b $F7, $3, $1, $D9, $13
+	dc.b $F2, $8, $1, $7D, $D7
+	dc.b $E2, $D, $1, $80, $DF
+	dc.b $FA, $6, $1, $88, $DF
+	dc.b $12, $0, $1, $8E, $DF
+	dc.b $D2, $D, $1, $8F, $E7
+	dc.b $CA, $8, $1, $97, $F7
+	dc.b $E2, $9, $1, $9A, $FF
+	dc.b $D2, $1, $1, $A0, $7
+	dc.b $CD, $2, $1, $ED, $B
+	dc.b $D5, $D, $1, $F0, $13
+	dc.b $E5, $0, $1, $F8, $23
+	dc.b $D5, $5, $1, $F9, $33
+	dc.b $D5, $0, $1, $FD, $43
+@internal_2F4: 	dc.b $13
+	dc.b $2F, $C, $1, $B4, $E3
+	dc.b $17, $D, $1, $B8, $EB
+	dc.b $27, $4, $1, $C0, $EB
+	dc.b $EF, $F, $1, $C2, $F3
+	dc.b $F, $C, $1, $D2, $F3
+	dc.b $17, $4, $1, $D6, $B
+	dc.b $1F, $0, $1, $D8, $B
+	dc.b $F7, $3, $1, $D9, $13
+	dc.b $F2, $8, $1, $7D, $D7
+	dc.b $E2, $D, $1, $80, $DF
+	dc.b $FA, $6, $1, $88, $DF
+	dc.b $12, $0, $1, $8E, $DF
+	dc.b $D2, $D, $1, $8F, $E7
+	dc.b $CA, $8, $1, $97, $F7
+	dc.b $E2, $9, $1, $9A, $FF
+	dc.b $D2, $1, $1, $A0, $7
+	dc.b $CF, $6, $1, $FE, $B
+	dc.b $D7, $9, $2, $4, $1B
+	dc.b $CF, $9, $2, $A, $33
+	even
