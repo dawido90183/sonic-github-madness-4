c7f63b56a462fdc7daaf751dec5cf3fb4fea351d
nanothehedghog <thenewuser0202@gmail.com>
2026-02-10 19:43:57 +0100

    fixed GHZ's plc

--- _inc/Pattern Load Cues.asm
+++ _inc/Pattern Load Cues.asm
@@ -83,7 +83,7 @@ PLC_GameOver:	dc.w ((PLC_GameOverend-PLC_GameOver-2)/6)-1
 ; ---------------------------------------------------------------------------
 PLC_GHZ:	dc.w ((PLC_GHZ2-PLC_GHZ-2)/6)-1
 		plcm	Nem_GHZ_1st, 0		; GHZ main patterns
-		plcm	Nem_GHZ_2nd, $39A0	; GHZ secondary patterns
+		plcm	Nem_GHZ_2nd, $1EA0	; GHZ secondary patterns
 		plcm	Nem_Stalk, $6B00	; flower stalk
 		plcm	Nem_PplRock, $7A00	; purple rock
 		plcm	Nem_Crabmeat, $8000	; crabmeat enemy
